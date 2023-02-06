9830a67d04161caa81f0fadb161bd50a0194b101
D-YF <daiyf1028@gmail.com>
2023-02-06 17:11:40 -0600

    from jpg/png to mp4

--- generate_video.py
+++ generate_video.py
@@ -13,7 +13,7 @@ for filename in path_list:
     img_array.append(img)
 
 out = cv2.VideoWriter('physical.avi',cv2.VideoWriter_fourcc(*'DIVX'), 5, size)
- 
+
 for i in range(len(img_array)):
     out.write(img_array[i])
 out.release()
